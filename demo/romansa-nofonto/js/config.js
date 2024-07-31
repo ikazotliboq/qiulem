128a8a07943a3574e89185f6c69406fcfceafab1
ikazotliboq <134859271+ikazotliboq@users.noreply.github.com>
2024-07-31 23:41:44 +0700

    Update config.js

--- demo/romansa-nofonto/js/config.js
+++ demo/romansa-nofonto/js/config.js
@@ -16,7 +16,7 @@
     for (var i = data.records.length - 1; i >= 0; i--) {
     var record = data.records[i];
     
-    outputHTML += "<div class='comment'><img src='https://i.ibb.co/HXXkhkj/g1.jpg' class='profile-picture'><div class='comment-details'><div class='am'>";
+    outputHTML += "<div class='comment'><img src='https://raw.githubusercontent.com/qobilcdn/resource/main/images-wedding/qobil_IMG_20240731_233550.png' class='profile-picture'><div class='comment-details'><div class='am'>";
     
     if (record.kehadiran === "Hadir") {
     outputHTML +=  "<h4 class='commenter-name fsub1'>" + record.nama + "<i style='color: #86682C; margin-left: 5px;'class='bi bi-check-circle-fill'></i></h4>";
