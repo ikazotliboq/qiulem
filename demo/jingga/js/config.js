9c679493a5cff2cf51886570c33302014ba222f8
ikazotliboq <134859271+ikazotliboq@users.noreply.github.com>
2024-07-31 20:18:02 +0700

    Update config.js

--- demo/jingga/js/config.js
+++ demo/jingga/js/config.js
@@ -16,7 +16,7 @@
     for (var i = data.records.length - 1; i >= 0; i--) {
     var record = data.records[i];
     
-    outputHTML += "<div class='comment'><img src='https://i.ibb.co/QFZycQg/g3.jpg' class='profile-picture'><div class='comment-details'><div class='am'>";
+    outputHTML += "<div class='comment'><img src='https://raw.githubusercontent.com/qobilcdn/resource/main/images-wedding/qobil_gn2.jpg' class='profile-picture'><div class='comment-details'><div class='am'>";
     
     if (record.kehadiran === "Hadir") {
     outputHTML +=  "<h4 class='commenter-name fsub1'>" + record.nama + "<i style='color: #AE713A; margin-left: 5px;'class='bi bi-check-circle-fill'></i></h4>";
