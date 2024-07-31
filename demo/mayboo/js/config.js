92da55203307497c5519f308852ece19656b942d
ikazotliboq <134859271+ikazotliboq@users.noreply.github.com>
2024-07-31 19:17:43 +0700

    Update config.js

--- demo/mayboo/js/config.js
+++ demo/mayboo/js/config.js
@@ -16,7 +16,7 @@
     for (var i = data.records.length - 1; i >= 0; i--) {
     var record = data.records[i];
     
-    outputHTML += "<div class='comment'><img src='https://i.ibb.co/svyt3H5/g9.jpg' class='profile-picture'><div class='comment-details'><div class='am'>";
+    outputHTML += "<div class='comment'><img src='https://raw.githubusercontent.com/qobilcdn/resource/main/images-wedding/qobil_g5.jpg' class='profile-picture'><div class='comment-details'><div class='am'>";
     
     if (record.kehadiran === "Hadir") {
     outputHTML +=  "<h4 class='commenter-name fsub1'>" + record.nama + "<i style='color: #D3AD6E; margin-left: 5px;'class='bi bi-check-circle-fill'></i></h4>";
